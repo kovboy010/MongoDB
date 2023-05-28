0901b4bb53b44ce30b5f9a43d4226385029ae945
kovboy010 <93874185+010kovboy010@users.noreply.github.com>
2023-05-28 14:12:30 +0300

    Реализован поиск записей в бд по шаблону.

--- MongoProjectPy/write_db.py
+++ MongoProjectPy/write_db.py
@@ -7,31 +7,31 @@ def write_in(as_x, as_y, as_z, angle_x, angle_y, angle_z, temp):
     data = {
         "Date": str(datetime.now().strftime('%d/%m/%Y')),
         "Time": str(datetime.now().strftime('%H:%M:%S')),
-        "acceleration X": as_x,
-        "acceleration Y": as_y,
-        "acceleration Z": as_z,
-        "angle X": angle_x,
-        "angle Y": angle_y,
-        "angle Z": angle_z,
-        "temperature": temp
+        "acceleration X": str(as_x),
+        "acceleration Y": str(as_y),
+        "acceleration Z": str(as_z),
+        "angle X": str(angle_x),
+        "angle Y": str(angle_y),
+        "angle Z": str(angle_z),
+        "temperature": str(temp)
     }
     print("ПОЛУЧИЛИ В data: ", data)
     cdb.start_base().insert_one(data)
 
-# a_x = 10
-# a_y = 20.12
-# a_z = 30.76
-# angl_x = 90.80
-# angl_y = 10.71
-# angl_z = 120.11
-# tem = 80.18
-#
-# a_x = a_x + 7
-# a_y = a_y + 7
-# a_z = a_z + 7
-# angl_x = angl_x + 7
-# angl_y = angl_y + 7
-# angl_z = angl_z + 7
-# tem = tem + 7
-#
-# write_in(a_x, a_y, a_z, angl_x, angl_y, angl_z, tem)
+a_x = 10
+a_y = 20.12
+a_z = 30.76
+angl_x = 90.80
+angl_y = 10.71
+angl_z = 120.11
+tem = 80.18
+
+# a_x = a_x + 5
+# a_y = a_y + 5
+# a_z = a_z + 5
+# angl_x = angl_x + 5
+# angl_y = angl_y + 5
+# angl_z = angl_z + 5
+# tem = tem + 5
+
+write_in(a_x, a_y, a_z, angl_x, angl_y, angl_z, tem)
